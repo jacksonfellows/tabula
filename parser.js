765503ab80cd6f914272e3a26bf1bb9633dbb7f0
Jackson Fellows <jtfellows@icloud.com>
2020-11-26 22:39:19 -0800

    basic expr parser

--- parser.js
+++ parser.js
@@ -66,7 +66,7 @@ function parseLatex(s) {
 					if (inp.consume() != '}') {
 						throw 'no closing } in \\text';
 					}
-					latex.push({type: 'text', text});
+					latex.push(literalToken(text));
 				} else {
 					var arguments = [];
 					while (inp.peek() == '{') {
@@ -74,7 +74,7 @@ function parseLatex(s) {
 						arguments.push(arg);
 						inp = newInp;
 					}
-					latex.push({type: 'command', command, arguments});
+					latex.push(...expandCommand(command, arguments));
 				}
 			}
 			else if (inp.peek() == '{') {
@@ -87,21 +87,169 @@ function parseLatex(s) {
 				while (isDigit(inp.peek())) {
 					n = n * 10 + charToDigit(inp.consume());
 				}
-				latex.push({type: 'number', value: n});
+				latex.push(literalToken(n));
 			} else if (isWhitespace(inp.peek())) {
 				// skip whitespace
 				inp.consume();
 			} else if (isAlpha(inp.peek())) {
 				// variable
-				latex.push({type: 'variable', variable: inp.consume()});
+				latex.push(literalToken(inp.consume()));
 			} else {
 				// operator
-				latex.push({type: 'operator', operator: inp.consume()});
+				latex.push(operatorToken(inp.consume()));
 			}
 		}
 		return [latex, inp];
 	}
 
-	return parseLatexInp(makeInp(s))[0];
+	var tokens = parseLatexInp(makeInp(s))[0];
+	tokens.push(endToken());
+	return tokens;
 }
 
+function expandCommand(command, arguments) {
+	switch (command) {
+	case 'frac':
+		if (arguments.length != 2) {
+			throw 'improper \\frac usage';
+		}
+		return [operatorToken('('), ...arguments[0], operatorToken(')'), operatorToken('/'), operatorToken('('), ...arguments[1], operatorToken(')')];
+	case 'sqrt':
+		if (arguments.length != 1) {
+			throw 'improper \\sqrt usage';
+		}
+		return [operatorToken('('), ...arguments[0], operatorToken(')'), operatorToken('^'), operatorToken('('), literalToken(1), operatorToken('/'), literalToken(2), operatorToken(')')];
+	case 'cdot':
+		return [operatorToken('*')];
+	default:
+		throw 'unsupported command: ' + command;
+	}
+}
+
+var token, next;
+
+function parseExpr(latex) {
+	next = makeInp(latex).consume;
+	token = next();
+	return expression();
+}
+
+function expression(rbp = 0) {
+	var t = token;
+	token = next();
+	var left = t.nud();
+	while (rbp < token.lbp) {
+		t = token;
+		token = next();
+		left = t.led(left);
+	}
+	return left;
+}
+
+function literalToken(value) {
+	return {
+		nud: function() {
+			return value;
+		}
+	};
+}
+
+function operatorToken(op) {
+	switch (op) {
+	case '+':
+		return operatorAddToken();
+	case '-':
+		return operatorSubToken();
+	case '*':
+		return operatorMulToken();
+	case '/':
+		return operatorDivToken();
+	case '^':
+		return operatorPowToken();
+	case '(':
+		return operatorLparenToken();
+	case ')':
+		return operatorRparenToken();
+	default:
+		throw 'unsupported operator: ' + op;
+	}
+}
+
+function operatorAddToken() {
+	return {
+		lbp: 10,
+		led: function(left) {
+			return ['+', left, expression(10)];
+		}
+	};
+}
+
+function operatorSubToken() {
+	return {
+		lbp: 10,
+		nud: function() {
+			return ['-', expression(100)];
+		},
+		led: function(left) {
+			return ['-', left, expression(10)];
+		}
+	};
+}
+
+function operatorMulToken() {
+	return {
+		lbp: 20,
+		led: function(left) {
+			return ['*', left, expression(20)];
+		}
+	};
+}
+
+function operatorDivToken() {
+	return {
+		lbp: 20,
+		led: function(left) {
+			return ['/', left, expression(20)];
+		}
+	};
+}
+
+function operatorPowToken() {
+	return {
+		lbp: 30,
+		led: function(left) {
+			return ['^', left, expression(30-1)];
+		}
+	};
+}
+
+function operatorLparenToken() {
+	return {
+		lbp: 0,
+		nud: function() {
+			var expr = expression();
+			if (!token.isRparen) {
+				throw 'expecting closing )';
+			}
+			token = next();
+			return expr;
+		}
+	};
+}
+
+function operatorRparenToken() {
+	return {
+		isRparen: true,
+		lbp: 0
+	};
+}
+
+function endToken() {
+	return {
+		lbp: 0
+	};
+}
+
+function parse(s) {
+	return parseExpr(parseLatex(s));
+}

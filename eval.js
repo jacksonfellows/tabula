858e0d4c165711f3114cac07059d9916fd5639ae
grantfellows <56371699+grantfellows@users.noreply.github.com>
2020-11-28 19:52:12 -0800

    basic ability to add new cells

--- eval.js
+++ eval.js
@@ -4,9 +4,11 @@ var mathSpan = document.getElementById('math');
 var parsed = document.getElementById('parsed');
 
 var config = {
-	handlers: { edit: function() {
-		parsed.textContent = JSON.stringify(parse(mathField.latex()));
-	}},
+	handlers: {
+		edit: function () {
+			parsed.textContent = JSON.stringify(parse(mathField.latex()));
+		}
+	},
 	spaceBehavesLikeTab: true,
 	leftRightIntoCmdGoes: 'up',
 	restrictMismatchedBrackets: true,
@@ -15,3 +17,13 @@ var config = {
 };
 
 var mathField = MQ.MathField(mathSpan, config);
+
+function addField() {
+	$("#fields").append("<p><span style=\"width: 100%\"></span></p>",
+		"<p>result</p>",
+		"<p><button>evaluate cell</button></p>");
+	var mathSpan = $("span:last")[0];
+	console.log($(this))
+	MQ.MathField(mathSpan, config);
+}
+

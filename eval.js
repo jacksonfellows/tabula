ed5ca0b80d5d690dd77144a638a2b8e38092b1ea
Jackson Fellows <jtfellows@icloud.com>
2020-11-30 07:43:17 -0800

    clean up ui code

--- eval.js
+++ eval.js
@@ -1,5 +1,5 @@
 var MQ = MathQuill.getInterface(2);
-var maxID = -1
+var maxID = -1;
 var mathFields = [];
 
 var config = {
@@ -11,10 +11,10 @@ var config = {
 };
 
 function addInputBox() {
-	var id = ++maxID
+	var id = ++maxID;
 	$("#fields").append("<p><span style=\"width: 100%\"></span></p>",
-		"<p id=\"O" + id + "\"></p>",
-		"<p><button onclick=\"updateOutputBox(" + id + ")\">evaluate cell</button></p>");
+	                    "<p id=\"O" + id + "\"></p>",
+	                    "<p><button onclick=\"updateOutputBox(" + id + ")\">evaluate cell</button></p>");
 	var mathSpan = $("span:last")[0];
 	mathFields.push(MQ.MathField(mathSpan, config));
 }

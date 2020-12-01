8d3601236745db339d4aa8536f0ec8cb5c543767
Jackson Fellows <jtfellows@icloud.com>
2020-12-01 09:17:51 -0800

    support some command name literals (many more to be added)

--- parser.js
+++ parser.js
@@ -137,6 +137,8 @@ function expandCommand(command, arguments) {
 		return [operatorToken(']')];
 	case 'equiv':
 		return [operatorToken('equiv')];
+	case 'ln': case 'log':
+		return [literalToken(command)];
 	default:
 		throw 'unsupported command: ' + command;
 	}
